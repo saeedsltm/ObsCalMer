8ed644e81a35602d1094488fd6b985e3917240c1
saeedsltm <saeed.sltm@gmail.com>
2022-08-17 23:32:00 +0430

    added new function for reviewing new picks

--- ObsCalMer.py
+++ ObsCalMer.py
@@ -1,20 +1,24 @@
-import obspy as obs
-from obspy.geodetics.base import gps2dist_azimuth as gps
-from obspy.core.event import Catalog
-from tqdm import tqdm
-from json import load
 import warnings
+from json import load
+
+import obspy as obs
+from obspy.core.event import Catalog
+from obspy.geodetics.base import gps2dist_azimuth as gps
+from tqdm import tqdm
+
 warnings.filterwarnings("ignore")
 
+# read config file
 with open("config.json") as f:
     configs = load(f)
 
-catRef = obs.read_events("cat1.dat")
-catCom = obs.read_events("cat2.dat")
-catTar = Catalog()
-catComEvents = Catalog()
-timeShift = configs["TimeShift"] # acceptable time shift between two common events (s)
-epicShift = configs["EpicentralShift"] #  acceptable epicenter shift between two common events (km)
+RefCat = obs.read_events(configs["ReferenceCatalogFileName"])
+ComCat = obs.read_events(configs["ComparedCatalogFileName"])
+TarCat = Catalog()
+ComEventsCat = Catalog()
+TimeShift = configs["TimeShift"]
+EpicShift = configs["EpicentralShift"]
+
 
 def computeDiff(RefEvent, ComEvent):
     """Compute time and epicenter difference between "reference" and "compared" events,
@@ -28,19 +32,91 @@ def computeDiff(RefEvent, ComEvent):
         bool: true if two events are common
     """
     RefEventPreferredOrigin = RefEvent.preferred_origin()
-    ComEventPreferredOrigin = ComEvent.preferred_origin()    
+    ComEventPreferredOrigin = ComEvent.preferred_origin()
     dT = abs(RefEventPreferredOrigin.time - ComEventPreferredOrigin.time)
     lat1 = RefEventPreferredOrigin.latitude
     lon1 = RefEventPreferredOrigin.longitude
     lat2 = ComEventPreferredOrigin.latitude
     lon2 = ComEventPreferredOrigin.longitude
     dD = abs(gps(lat1=lat1, lon1=lon1, lat2=lat2, lon2=lon2)[0]*1e-3)
-    if dT < timeShift and dD < epicShift:
+    if dT < TimeShift and dD < EpicShift:
         return True
     else:
         return False
 
-def updateEvent(RefEvent, ComEvent, numNewPhaseP, numNewPhaseS, numRegardedPhases):
+
+def readExtra(pick):
+    """reads pick weight 
+
+    Args:
+        pick (event.pick): an obspy event pick
+
+    Returns:
+        str: pick weight 
+    """
+
+    try:
+        weight = pick.extra.get("nordic_pick_weight", "0")
+        if isinstance(weight, type({})):
+            weight = weight["value"]
+    except AttributeError:
+        weight = "0"
+    return weight
+
+
+def manageNewPicks(RefEventPicks, ComEventPicks, numRegardedPhases):
+    """Manage new picks
+
+    Args:
+        RefEventPicks (event.picks): an obspy event picks, for "reference" event,
+        ComEventPicks (event.picks): an obspy event picks, for "compared" event,
+        numRegardedPhases (int): number of regarded pick phases
+
+    Returns:
+        _type_: _description_
+    """
+    RefEventPickRemarks = ["_".join(
+        [pick.waveform_id.station_code, pick.phase_hint, readExtra(pick)]) for pick in RefEventPicks]
+    ComEventPickRemarks = ["_".join(
+        [pick.waveform_id.station_code, pick.phase_hint, readExtra(pick)]) for pick in ComEventPicks]
+    newPicks = list(set(ComEventPickRemarks) - set(RefEventPickRemarks))
+    numRegardedPhases += len(ComEventPickRemarks) - len(newPicks)
+    newPicksIndex = [ComEventPickRemarks.index(
+        newPick) for newPick in newPicks]
+    return newPicksIndex, numRegardedPhases
+
+
+def ReviewEventPicks(event):
+    """Review event picks
+
+    Args:
+        event (obspy.event): an obspy event picks, for updated "reference" event,
+
+    Returns:
+        obspy.event: _description_
+    """
+    IgnoredResource_id = []
+    for trialPick in event.picks:
+        s, p, w = trialPick.waveform_id.station_code, trialPick.phase_hint, readExtra(
+            trialPick)
+        for pick in event.picks:
+            ss, pp, ww = pick.waveform_id.station_code, pick.phase_hint, readExtra(
+                pick)
+            if (s, p) == (ss, pp) and float(w) > float(ww):
+                IgnoredResource_id.append(trialPick.resource_id)
+    for i, pick in enumerate(event.picks):
+        if pick.resource_id in IgnoredResource_id:
+            event.picks.pop(i)
+    for i, amplitude in enumerate(event.amplitudes):
+        if amplitude.resource_id in IgnoredResource_id:
+            event.amplitudes.pop(i)
+    for i, arrival in enumerate(event.preferred_origin().arrivals):
+        if arrival.pick_id in IgnoredResource_id:
+            event.preferred_origin().arrivals.pop(i)
+    return event
+
+
+def updateEvent(RefEvent, ComEvent, numNewPhaseP, numNewPhaseS, numRegardedPhases, numNewAmplitudes):
     """Update "reference" event with new phases comes from "compared" event
 
     Args:
@@ -57,48 +133,64 @@ def updateEvent(RefEvent, ComEvent, numNewPhaseP, numNewPhaseS, numRegardedPhase
     ComEventPreferredOrigin = ComEvent.preferred_origin()
     RefEventPicks = RefEvent.picks
     ComEventPicks = ComEvent.picks
+    RefEventAmplitudes = RefEvent.amplitudes
+    ComEventAmplitudes = ComEvent.amplitudes
     RefEventArrivals = RefEventPreferredOrigin.arrivals
     ComEventArrivals = ComEventPreferredOrigin.arrivals
-    RefEventPickRemarks = ["_".join([pick.waveform_id.station_code, pick.phase_hint]) for pick in RefEventPicks]
-    ComEventPickRemarks = ["_".join([pick.waveform_id.station_code, pick.phase_hint]) for pick in ComEventPicks]
-    newPicks = list(set(ComEventPickRemarks) - set(RefEventPickRemarks))
-    numRegardedPhases += len(ComEventPickRemarks) - len(newPicks)
-    newPicksIndex = [ComEventPickRemarks.index(newPick) for newPick in newPicks]
+    newPicksIndex, numRegardedPhases = manageNewPicks(
+        RefEventPicks, ComEventPicks, numRegardedPhases)
     for newPickIndex in newPicksIndex:
         newPickResource_id = ComEventPicks[newPickIndex].resource_id
-        newArrivals = [arrival for arrival in ComEventArrivals if arrival.pick_id == newPickResource_id]
+        newArrivals = [
+            arrival for arrival in ComEventArrivals if arrival.pick_id == newPickResource_id]
+        newAmplitudes = [
+            amplitude for amplitude in ComEventAmplitudes if amplitude.pick_id == newPickResource_id]
         RefEventPicks.append(ComEventPicks[newPickIndex])
         RefEventArrivals.extend(newArrivals)
+        RefEventAmplitudes.extend(newAmplitudes)
         if "P" in ComEventPicks[newPickIndex].phase_hint:
             numNewPhaseP += 1
         elif "S" in ComEventPicks[newPickIndex].phase_hint:
-            numNewPhaseS += 1            
+            numNewPhaseS += 1
+        elif "AML" in ComEventPicks[newPickIndex].phase_hint:
+            numNewAmplitudes += 1
     RefEvent.picks = RefEventPicks
     RefEvent.preferred_origin().arrivals = RefEventArrivals
-    return RefEvent, numNewPhaseP, numNewPhaseS, numRegardedPhases
+    if configs["ReplaceNewPhaseWithHigherWeight"]:
+        RefEvent = ReviewEventPicks(RefEvent)
+    return RefEvent, numNewPhaseP, numNewPhaseS, numRegardedPhases, numNewAmplitudes
+
 
 # starts application
 numCommonEvents = 0
 numNewPhaseP = 0
 numNewPhaseS = 0
+numNewAmplitudes = 0
 numRegardedPhases = 0
 
 print("+++ Starts merging catalogs...")
-for eventID, RefEvent in enumerate(tqdm(catRef)):
-    for ComEvent in catCom:
+for eventID, RefEvent in enumerate(tqdm(RefCat)):
+    for ComEvent in ComCat:
         if computeDiff(RefEvent, ComEvent):
-            RefEvent, numNewPhaseP, numNewPhaseS, numRegardedPhases = updateEvent(RefEvent, ComEvent, numNewPhaseP, numNewPhaseS, numRegardedPhases)
-            catComEvents.append(RefEvent)
+            RefEvent, numNewPhaseP, numNewPhaseS, numRegardedPhases, numNewAmplitudes = updateEvent(
+                RefEvent, ComEvent, numNewPhaseP, numNewPhaseS, numRegardedPhases, numNewAmplitudes)
+            ComEventsCat.append(RefEvent)
             numCommonEvents += 1
             break
-    catTar.append(RefEvent)
+    TarCat.append(RefEvent)
 
-catTar.write("updatedCatalog.dat", format="NORDIC")
+TarCat.write("updatedCatalog.dat", format="NORDIC")
 if configs["OutputCommonEventsCatalog"]:
-    catComEvents.write("commonEventsCatalog.dat", format="NORDIC")
+    ComEventsCat.write("commonEventsCatalog.dat", format="NORDIC")
 
 # summary
-print("+++ Number of common events: {numCommonEvents}".format(numCommonEvents=numCommonEvents))
-print("+++ Number of new P phases: {numNewPhaseP}".format(numNewPhaseP=numNewPhaseP))
-print("+++ Number of new S phases: {numNewPhaseS}".format(numNewPhaseS=numNewPhaseS))
-print("+++ Number of regarded phases: {numRegardedPhases}".format(numRegardedPhases=numRegardedPhases))
+print(
+    "+++ Number of common events: {numCommonEvents}".format(numCommonEvents=numCommonEvents))
+print(
+    "+++ Number of new P phases: {numNewPhaseP}".format(numNewPhaseP=numNewPhaseP))
+print(
+    "+++ Number of new S phases: {numNewPhaseS}".format(numNewPhaseS=numNewPhaseS))
+print("+++ Number of new Amplitudes: {numNewAmplitudes}".format(
+    numNewAmplitudes=numNewAmplitudes))
+print("+++ Number of regarded phases: {numRegardedPhases}".format(
+    numRegardedPhases=numRegardedPhases))
